d42e5d85550f3421c2bdaa0ed05fdb2d1e93bf98
victor <vfinamore@gmail.com>
2019-01-28 14:18:18 -0200

    Mensagem ok

---
+++ src/application/Bomdia.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bomdia {
+
+	public static void main(String[] args) {
+		
+	
+		
+		System.out.println("Bom dia!!!");
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
